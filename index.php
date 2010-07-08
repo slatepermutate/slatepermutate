0eb19191497d34fa988ff0dbe4b33992b785823b
ethanzonca <none@none>
2010-07-08 18:47:28 -0400

    Test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-<title>Scheduler</title>
+<title>Scheduler - Beta</title>
 
 <link rel="stylesheet" href="styles/general.css" type="text/css" media="screen" charset="utf-8">
 
