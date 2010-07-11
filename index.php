9492dd5e4e2caf04fa6abb014718c0c5809ff0fd
nathang <ngelderloos7@gmail.com>
2010-07-11 09:38:40 -0400

    I'm bored

--- index.php
+++ index.php
@@ -47,8 +47,8 @@
 </div>
 
 <div id="footer">
-<h5>&copy; <?php echo Date("Y"); ?> <a href="http://protofusion.org/~nathang/">Nathan Gelderloos</a><br />
-with special thanks to <a href="http://ethanzonca.com">Ethan Zonca</a></h5>
+<h5>&copy; <?php echo Date("Y") . ' ' . Date("n") . ' ' . Date("j"); ?> <a href="http://protofusion.org/~nathang/">Nathan Gelderloos</a><br />
+with special thanks to <a href="http://ethanzonca.com">Ethan <?php if((Date("n")==7)&&(Date("j")==11)){echo '"the Birthday Boy" ';} ?>Zonca</a></h5>
 </div>
 
 </body>
