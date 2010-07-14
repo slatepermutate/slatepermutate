379e6cb5c61ceb328a190826ac64b43936d612b8
ethanzonca <none@none>
2010-07-13 23:25:30 -0400

    Migrated index to page class

--- index.php
+++ index.php
@@ -1,57 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-<head>
-<title>Scheduler</title>
+<?php 
+	include 'inc/class.page.php'; 
+	$mypage = new page('Scheduler');
+?>
 
-<link rel="stylesheet" href="styles/general.css" type="text/css" media="screen" charset="utf-8">
-
-<style type="text/css">
-  .error {
-    color: #F00; /* For validation errors */
-  } 
-  .error p {
-    padding: 0;
-    margin: 0;
-    font-size: .7em;
-  }
-  td.center {
-    text-align:center;
-  }
-  .section:nth-child(even) {
-    background: #DDDDDD;
-  }
-  .none {
-    background: #fff;
-  }
-  .class {
-    background: #99FF99;
-  }
-  #classage {
-    float:left;
-  }
-  .paddingtop {
-    padding: .5em;
-    padding-top: .8em;
-  }
-</style>
-</head>
-<body>
-
-<?php include_once("analyticstracking.php") ?>
-
-<div id="header">
-<h1><em>SlatePermutate</em> -  Scheduler</h1>
-</div>
-
-<div id="content">
 <h3>Find the schedule that works for you!</h3>
 <p>View a <a href="schedulecreator.php">demo output</a> or get <a href="input.php">started on your own</a>. This program was created specifically for use by <a href="http://www.calvin.edu">Calvin College</a> students, although it may work for other schools as well.</p>
-</div>
 
-<div id="footer">
-<h5>&copy; <?php echo Date("Y") . ' ' . Date("n") . ' ' . Date("j"); ?> <a href="http://protofusion.org/~nathang/">Nathan Gelderloos</a><br />
-with special thanks to <a href="http://ethanzonca.com">Ethan <?php if((Date("n")==7)&&(Date("j")==11)){echo '"the Birthday Boy" ';} ?>Zonca</a></h5>
-</div>
 
-</body>
-</html>
+<?
+$mypage->foot();
+?>
