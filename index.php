bbc82ec4655c27305e08e33a393b7f7beef13fe7
ethanzonca <none@none>
2010-09-25 01:26:29 -0400

    Made text frendlier-soundingish

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
-	include 'inc/class.page.php'; 
+	include_once 'inc/class.page.php'; 
 	$mypage = new page('Scheduler');
 ?>
 
 <h3>Find the schedule that works for you!</h3>
-<p>View a <a href="schedulecreator.php">demo output</a> or get <a href="input.php">started on your own</a>. This program was created specifically for use by <a href="http://www.calvin.edu">Calvin College</a> and <a href="http://cedarville.edu/">Cedarville University</a> students, although it may work for other schools as well.</p>
+<p>View <a href="schedulecreator.php">demo output</a> or get <a href="input.php">started on your own</a>. This program was created by <a href="http://www.calvin.edu">Calvin College</a> and <a href="http://cedarville.edu/">Cedarville University</a> students. SlatePermutate is institution-apathetic.</p>
 
 
 <?php
