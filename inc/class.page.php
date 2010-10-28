24c3cd640d6f4e8058654949adc348c0fe6ce061
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2010-10-28 19:31:32 -0400

    Upgrade to jQuery-1.4.3 (or any other future jQuery-1.4, because we now just use a more generalized URI).

--- inc/class.page.php
+++ inc/class.page.php
@@ -52,7 +52,7 @@ class page
     require_once('school.inc');
 
     // Scripts and styles available to include
-    $this->headCode['jQuery'] = '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>';
+    $this->headCode['jQuery'] = '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript"></script>';
     $this->headCode['jQueryUI'] = '<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js" type="text/javascript"></script><link rel="stylesheet" href="styles/jqueryui.css" type="text/css" media="screen" charset="utf-8" />';
     $this->headCode['jValidate'] = '<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.pack.js"></script>';
     $this->headCode['schedInput'] = '<script type="text/javascript" src="scripts/scheduleInput.js"></script>';
