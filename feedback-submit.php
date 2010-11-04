52ef761a328f4067f8d9c3307a84eeb9b5fecb1a
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2010-11-04 10:26:05 -0400

    Actually reject bad input for the feedback form instead of detecting it but still allowing it through.

--- feedback-submit.php
+++ feedback-submit.php
@@ -21,20 +21,26 @@ $school_id = isset($_SESSION['school']) ? $_SESSION['school'] : '';
 $feedback = $_POST['feedback'];
 $rating = $_POST['rating'];
 
+$reject = FALSE;
+
 if (eregi('http:', $feedback)) { 
   echo 'Please do not include URLs in your submission! Please click "back" and try again.';
+  $reject = TRUE;
 }
-if((!$visitormail == '') && (!strstr($visitormail, '@') || !strstr($visitormail, '.'))) {
+if (empty($visitormail) || !preg_match('/^[^@]+@[^@]+\.[^@]+$/', $visitormail)) {
   echo '<p>Please click "back" and enter valid e-mail address.</p>';
+  $reject = TRUE;
 }
 if(empty($nameis) || empty($feedback) || empty($visitormail)) {
   echo '<p>Please click "back" and fill in all fields.</p>';
+  $reject = TRUE;
 }
 
+if (!$reject)
+  {
+    $feedback = stripcslashes($feedback);
 
-$feedback = stripcslashes($feedback);
-
-$message = date('l, F j, Y, g:i a') ."
+    $message = date('l, F j, Y, g:i a') ."
 From: $nameis ($visitormail)
 School: $school ($school_id)\n
 Rating: $rating 
@@ -45,14 +51,13 @@ Browser = $httpagent
 Deployment = $fromdom
 ";
 
-$from = "From: $visitormail\r\n";
+    $from = "From: $visitormail\r\n";
 
-/* $feedback_emails has its default set in inc/class.page.inc, can be set in config.inc */
-foreach($feedback_emails as $toaddr)
-  {
-    mail($toaddr, $subject, $message, $from);
-  }
-
+    /* $feedback_emails has its default set in inc/class.page.inc, can be set in config.inc */
+    foreach($feedback_emails as $toaddr)
+      {
+	mail($toaddr, $subject, $message, $from);
+      }
 
 ?>
 
@@ -61,4 +66,6 @@ foreach($feedback_emails as $toaddr)
 
 
 <?php
+  }
+
   $feedbackpage->foot();
