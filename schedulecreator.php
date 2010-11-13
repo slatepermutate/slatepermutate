9cc5868b0e345883864eaf8cd357cc1d87a4ec1e
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2010-11-13 01:29:51 -0500

    Use the Affero General Public License, as agreed upon by Ethan Zonca, Nathan Gelderloos, and Nathan Phillip Brink (myself).

--- schedulecreator.php
+++ schedulecreator.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Copyright 2010 Nathan Gelderloos, Ethan Zonca, Nathan Phillip Brink
+ *
+ * This file is part of SlatePermutate.
+ *
+ * SlatePermutate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SlatePermutate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SlatePermutate.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 include_once 'class.schedule.php';
 include_once 'class.class.php';
