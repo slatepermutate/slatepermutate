95093b1caf91f0a4e1f068b46c778d5756e3d9fb
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2011-03-29 21:06:39 -0400

    Prevent the user from deleting the one empty course.

--- scripts/scheduleInput.js
+++ scripts/scheduleInput.js
@@ -481,9 +481,18 @@ jQuery(document).ready(function() {
 	// Deletes the selected class from input
 	//--------------------------------------------------
 	jQuery('.deleteClass').live('click', function() {
-		if(confirm('Delete class and all sections of this class?')) {
-		    course_remove(jQuery(this).parent().parent().data('course_i'));
-		}
+	    /* The user is not allowed to interactively delete the one empty course */
+	    var course_i = jQuery(this).parent().parent().data('course_i');
+	    if (slate_permutate_course_free == course_i)
+		return false;
+	    if(confirm('Delete class and all sections of this class?')) {
+		/* The one empty course may have became this course in that time */
+		if (slate_permutate_course_free == course_i)
+		    return false;
+		course_remove(course_i);
+		return false;
+	    }
+	    return false;
 	});
 
 	//--------------------------------------------------
